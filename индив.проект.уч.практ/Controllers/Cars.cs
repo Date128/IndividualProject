e9eec4e77043fcea05e3c573d55beb34026a9c88
Date128 <an.podobyeva@mail.ru>
2024-01-24 09:35:10 +0300

    Individ Project
    Project

---
+++ индив.проект.уч.практ/Controllers/Cars.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace индив.проект.уч.практ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        public CarsContext Context { get; }
+
+        public CarsController(CarsContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() // Получение всех записей
+        {
+            List<User> user = Context.Users.ToList();
+            return Ok(user);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) // Получение одной записи
+        {
+            User? user = Context.Users.Where(x => x.Id == id).FirstOrDefault();
+            if (user == null)
+            {
+                return BadRequest("Not Found");
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user) // Создание одной записи
+        {
+            Context.Users.Add(user);
+            Context.SaveChanges();
+            return Ok(user);
+        }
+
+        [HttpPut]
+        public IActionResult Update(User user) // Изменение существующей записи
+        {
+            Context.Users.Update(user);
+            Context.SaveChanges();
+            return Ok(user);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id) // Удаление существующей записи
+        {
+            User? user = Context.Users.Where(x => x.Id == id).FirstOrDefault();
+            if (user == null)
+            {
+                return BadRequest("Not Found");
+            }
+            Context.Users.Remove(user);
+            Context.SaveChanges();
+            return Ok(user);
+        }
+    }
+}
+    }
+}
